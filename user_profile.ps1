c13aa51bccd42d277c13447c7aba6f7eab42e3fc
LevisCoder <khoa05032006@gmail.com>
2022-05-21 21:36:57 +0700

    Update

--- user_profile.ps1
+++ user_profile.ps1
@@ -1,3 +1,11 @@
+# PSReadLine
+Import-Module -Name PSReadLine
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineOption -BellStyle none
+Set-PSReadLineKeyHandler -Key Ctrl-j -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key Ctrl-k -Function HistorySearchForward
+
 # Icons
 Import-Module -Name Terminal-Icons
 
