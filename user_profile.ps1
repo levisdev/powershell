bc60be8852c7783fa7ca9721734a0546411249b1
levisdev <khoa05032006@gmail.com>
2022-07-16 20:07:17 +0700

    Remove vi alias

--- user_profile.ps1
+++ user_profile.ps1
@@ -9,8 +9,5 @@ Set-PSReadLineKeyHandler -Key Ctrl-k -Function HistorySearchForward
 # Icons
 Import-Module -Name Terminal-Icons
 
-# Alias
-Set-Alias vi nvim
-
 $ENV:STARSHIP_CONFIG = "$HOME\.config\powershell\starship.toml"
 Invoke-Expression (&starship init powershell)
